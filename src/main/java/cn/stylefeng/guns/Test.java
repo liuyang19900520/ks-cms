97966e82f322185b456b02aac19b8e381e67a723
liuyang19900520 <liuyang19900520@hotmail.com>
2019-05-30 18:56:57 +0900

    考勤总汇功能第一版

---
+++ src/main/java/cn/stylefeng/guns/Test.java
@@ -0,0 +1,21 @@
+package cn.stylefeng.guns;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class Test {
+
+    /**
+     * accessCode : testtest
+     * terminalCode : 0002
+     * storeCode : 0000031850
+     * barCode : 0000000000104
+     */
+
+    private String accessCode;
+    private String terminalCode;
+    private String storeCode;
+    private String barCode;
+}
