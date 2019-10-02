623aee38f571379b141793394b6824a17fc0e82c
liuyang19900520 <liuyang19900520@hotmail.com>
2019-10-02 23:32:00 +0900

    丰富员工管理

--- src/main/java/cn/stylefeng/guns/modular/system/model/UserDto.java
+++ src/main/java/cn/stylefeng/guns/modular/system/model/UserDto.java
@@ -1,15 +1,15 @@
 /**
  * Copyright 2018-2020 stylefeng & fengshuonan (https://gitee.com/stylefeng)
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License; Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing; software
+ * distributed under the License is distributed on an "AS IS" BASIS;
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND; either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -43,5 +43,18 @@ public class UserDto {
     private Long deptId;
     private String status;
     private String avatar;
+
+    private String employeeId;
+    private String employeeNameCN;
+    private String employeeNameJP;
+    private String employeeNameKANA;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date entryTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date brithdate;
+    private String  province;
+    private String  city;
+    private String station;
+    private String  address;
 
 }
